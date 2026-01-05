302ba20d8d5146d7899566eafb4e5d999f8cb3fc
eunyoung1111 <blahblah_y@naver.com>
2026-01-05 15:44:28 +0900

    Update run_process.sh

--- script/run_process.sh
+++ script/run_process.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#cd /home/ubuntu/script
-#docker compose up -d
-
-docker compose -f /home/ubuntu/script/docker-compose.yml up || true
+cd /home/ec2-user/script
+docker-compose down
+docker-compose up -d
